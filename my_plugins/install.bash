4ad5aeb308656c3a7a8a97aee6747837b87cc3e9
Jomel S. Araniego <jomel_araniego@ITSs-MacBook-Pro.local>
2021-04-10 17:46:43 +0800

    Add installer

---
+++ my_plugins/install.bash
@@ -0,0 +1,4 @@
+git clone https://github.com/tomasr/molokai
+git clone https://github.com/tomasr/vim-jsx
+git clone https://github.com/tomasr/vim-makefile
+git clone https://github.com/tomasr/vim-tsx
